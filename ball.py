998ca115c24bec67f49b5bc269720b50bf740f99
rosauda <rodrigo.sauda@hotmail.com>
2023-01-15 16:31:26 +0100

    done

--- ball.py
+++ ball.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import time
 
 
 class Ball(Turtle):
@@ -7,10 +8,29 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
 
     def ball_move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def ball_bounce_y(self):
+        self.y_move *= -1
+
+    def ball_bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.ball_bounce_x()
+
+
+
+
 
 
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(width=1000, height=600)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("My Pong game")
 screen.tracer(0)
 
-r_paddle = Paddle((450, 0))
-l_paddle = Paddle((-450, 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
@@ -21,9 +23,26 @@ screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.ball_move()
 
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.ball_bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.ball_bounce_x()
+
+    # Detect when r_paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect when l_paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
